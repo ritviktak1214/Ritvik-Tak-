acbaf9d4390e8bc38c820fe32405b71c82183396
Ritvik Tak <thakur.ritvik9440@gmail.com>
2025-06-16 19:54:10 +0530

    file fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-  ],
+  base: './',  // ✅ Add this line
+  plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 1500, // optional
+    chunkSizeWarningLimit: 1500,
     rollupOptions: {
       output: {
         manualChunks(id) {
@@ -18,4 +18,3 @@ export default defineConfig({
     }
   }
 })
-
